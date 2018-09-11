affa39740aae4a5aed86d5ec9e8950942d7398db
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-09-11 23:43:29 +0200

    better presentation

--- src/components/ManifestDesigner.vue
+++ src/components/ManifestDesigner.vue
@@ -1,15 +1,27 @@
 <template>
   <multipane class="vertical-panes" layout="vertical">
-  <div class="pane" :style="{ minWidth: '100px', width: '50%', maxWidth: '50%' }">
+  <div class="pane manifest-form" :style="{ minWidth: '100px', width: '50%', maxWidth: '50%', textAlign: 'right' }">
     <div>
+      <label>نوع سرویس</label>
+      <select v-model="manifest.kind">
+        <option value="ExternalService">External Service</option>
+        <option value="InternalService">Internal Service</option>
+        <option value="ManagedService">Managed Service</option>
+      </select>
+    </div>
+    <div>
+      <label>نام سرویس</label>
+      <input type="text" v-model="manifest.name"/>
+    </div>
+    <div>
+      <label>پورت سرویس</label>
+      <input type="number" min="1" v-model.number="manifest.spec.port" />
+    </div>
+    <div>
+      <label>اندازه رپلیکا</label>
       <input type="number" min="1" v-model.number="manifest.spec.replicas" />
-      <input type="button"  v-on:click="delete manifest.spec.replicas" value="x" />
     </div>
 
-    <div>
-      <input type="button" v-on:click="manifest.spec.volumes = undefined" value="x" />
-      <input type="button" v-on:click="manifest.spec.volumes = [{}]" value="y"/>
-    </div>
     <div>
       <input type="text" v-model="manifest.spec.image" />
     </div>
@@ -20,17 +32,12 @@
     <div>
       {{ manifest }}
     </div>
-    <div>
-      <textarea v-text="yamljs.dump(manifest)" v-model="x" />
-      <textarea v-text="jsyml.dump(manifest)"  />
-    </div>
   </div>
   <multipane-resizer></multipane-resizer>
   <div class="pane" :style="{ flexGrow: 1 }">
       <editor v-model="yml" @init="editorInit" lang="yaml" theme="twilight" width="100%" height="100%"></editor>
   </div>
 </multipane>
-  </div>
 </template>
 
 <script>
@@ -39,7 +46,7 @@ import jsyml from 'js-yaml'
 import { Multipane, MultipaneResizer } from 'vue-multipane'
 
 export default {
-  name: 'HelloWorld',
+  name: 'ManifestDesigner',
   data () {
     return {
       x: '',
@@ -56,17 +63,18 @@ export default {
   },
   computed: {
     yml: function () {
-      return yamljs.dump(this.manifest)
+      console.log('manifest', this.manifest)
+      return jsyml.safeDump(this.manifest, {sortKeys: true, skipInvalid: true})
     }
   },
   methods: {
     editorInit: function () {
-      require('brace/ext/language_tools') //language extension prerequsite...
+      require('brace/ext/language_tools')
       require('brace/mode/html')
-      require('brace/mode/javascript')    //language
+      require('brace/mode/javascript')
       require('brace/mode/less')
       require('brace/theme/twilight')
-      require('brace/snippets/javascript') //snippet
+      require('brace/snippets/javascript')
     }
   },
   components: {
@@ -97,18 +105,22 @@ export default {
     color: #42b983;
   }
 
-.vertical-panes {
-  width: 100%;
-  height: 400px;
-  border: 1px solid #ccc;
-}
-.vertical-panes > .pane {
-  text-align: left;
-  padding: 15px;
-  overflow: hidden;
-  background: #eee;
-}
-.vertical-panes > .pane ~ .pane {
-  border-left: 1px solid #ccc;
-}
+  .vertical-panes {
+    width: 100%;
+    height: 400px;
+    border: 1px solid #ccc;
+  }
+  .vertical-panes > .pane {
+    text-align: left;
+    padding: 15px;
+    overflow: hidden;
+    background: #eee;
+  }
+  .vertical-panes > .pane ~ .pane {
+    border-left: 1px solid #ccc;
+  }
+
+  .manifest-form {
+    text-align: right;
+  }
 </style>
